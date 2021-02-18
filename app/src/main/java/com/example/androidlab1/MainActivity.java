7108114b3a19deca6fbe946a49af6fda64df4bd9
Kirkmania <george.kirkman27@gmail.com>
2021-02-18 19:10:37 +0000

    Initial commit to Git of post-lab2 application

---
+++ app/src/main/java/com/example/androidlab1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.androidlab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickUpdate(View view) {
+        TextView currentgreeting = (TextView) findViewById(R.id.textView);
+        Spinner greetings = (Spinner) findViewById(R.id.spinner);
+        currentgreeting.setText(greetings.getSelectedItem().toString());
+    }
+}/**/
